b1459fa9b6dd6e3226d1016f6852a62c7a10d8af
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2020-12-16 07:21:47 +0100

    Remake of code
    Some functions and features missing, like ifequal, ifless and ifbigger, they will be replaced

--- ezcode.py
+++ ezcode.py
@@ -1,31 +1,37 @@
+### Startup variables, only alivalable here
+debug = True
 
-#EZLANG INTERPRETER
-#Made by khhs
-#For use in cmd
 
-import string as stringmod
-import tkinter
+### Imports
 import sys
-import os
+import tkinter
 from tkinter import filedialog
-x = 500
-sys.setrecursionlimit(x)
-string = stringmod.ascii_letters
-funcname = string
-statements = string
-func = [funcname, "(", statements, ");"]
 
+### IMPORTANT VARIABLES
+
+recursion = 100
+variables = {} #The variable list, contains different types
+code = [] #The script to debug
+
+### Open file
 noa = len(sys.argv)
 args = sys.argv
 root = tkinter.Tk()
 root.withdraw()
-if noa == 1:
+if debug:
+    compilefile = "test.ez"
+elif noa == 1:
     compilefile = filedialog.askopenfilename()
 else:
     #print(args[1])
     compilefile = args[1]
 
-code = open(compilefile, "r").read()
+if not (compilefile == ""):
+    code = open(compilefile, "r").read()
+else:
+    code = 'Write("No file specified!");\n'
+
+### Remove newlines and split at semicolons
 
 code2  = code.split("\n")
 newCode = []
@@ -33,167 +39,45 @@ for p in code2:
     newCode.append(p.lstrip())
 code2 = newCode
 code = ''.join(code2).split(";")
-write = func
-write[0] = "write"
-write[2] = string
-root.destroy()
-variables = {}
+code = code[:len(code)-1]
 
-funcs = {}
-oldcode = code
-code = []
-for checkline in oldcode:
-    if checkline != "":
-        if checkline[:1] == "\n":
-            checkline = checkline[1:]
-        code.append(checkline)
 
-def clear(): 
-  
-    # for windows 
-    if os.name == 'nt': 
-        _ = os.system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = os.system('clear') 
+### Define some important functions
 
-def execode():
-    global code
-    global variables
-    for line in code:
-        if(line[0] != "#"):
-            if "function" in line:
-                cmds = line.split("{")[1].split("}")[0].split(":")
-                #print(cmds)
-                if " {" in line:
-                    funcs[line[len("function")+1:line.index("{")-1] + "()"] = cmds
-                else:
-                    funcs[line[len("function")+1:line.index("{")] + "()"] = cmds
-                #print(funcs)
-            elif "Write(" in line and line[6] == '"' and line[len(line) - 2] == '"':
-                print(line[7:len(line) - 2])
-            elif "Write(" in line:
-                calc = str(line)[6:len(str(line))-1]
-                result = "error, faulty equation!"
-                if(calc in variables.keys()):
-                    result = variables[calc]
-                elif calc.split(" ")[1] in ["+", "-", "*", "/", "^"]:
-                    calc = calc.split(" ")
-                    if (calc[1] == "+"):
-                        result = int(calc[0]) + int(calc[2])
-                    elif (calc[1] == "-"):
-                        result = int(calc[0]) - int(calc[2])
-                    elif (calc[1] == "*"):
-                        result = int(calc[0]) * int(calc[2])
-                    elif (calc[1] == "/"):
-                        result = int(calc[0]) / int(calc[2])
-                    elif (calc[1] == "^"):
-                        result = int(calc[0]) ** int(calc[2])
-                print(result)
-            elif "<-" in line:
-                lineToWork = line.split("<-")
-                inp = lineToWork[1][1:]
-                if ("Input()" in line):
-                    inp = input()
-                variables[''.join(lineToWork[0][0:len(lineToWork[0])-1])] = inp
-                #print(variables)
+def execLine(line):
+    global recursion
+    sys.setrecursionlimit(recursion)
+    recursion += 1
 
-            elif line in funcs.keys():
-                #print(funcs[line])
-                for cmd in funcs[line]:
-                    execodeline(cmd)
-            elif "python" in line:
-                eval(line[len("python")+1:])
-            elif "Input()" in line:
-                input()
-            elif "<-" in line:
-                lineToWork = line.split("<-")
-                #print(lineToWork)
-                if ("Input()" in line):
-                    inp = input()
-                variables[''.join(lineToWork[0][0:len(lineToWork[0])-1])] = inp
-                #print(variables)
+    if(line[0] == "#"):
+        return
 
-            elif "ifequal" in line:
-                lineForWork = line[len("if"):].split("{")[1].split(",")
-                if (int(variables[lineForWork[0]]) == int(lineForWork[1])):
-                    execodeline(lineForWork[2][1:len(lineForWork[2])-1])
-            elif "ifless" in line:
-                lineForWork = line[len("if"):].split("{")[1].split(",")
-                if (int(variables[lineForWork[0]]) < int(lineForWork[1])):
-                    execodeline(lineForWork[2][1:len(lineForWork[2])-1])
-
-            elif line[:len(line)-2] in variables.keys() and "++" in line:
-                variables[line[:len(line)-2]] = int(variables[line[:len(line)-2]]) + 1
-            elif line[:len(line)-2] in variables.keys() and "--" in line:
-                variables[line[:len(line)-2]] = int(variables[line[:len(line)-2]]) - 1
-            
-            elif line in funcs.keys():
-                #print(funcs[line])
-                for cmd in funcs[line]:
-                    execodeline(cmd)
-            elif "python" in line:
-                eval(line[len("python")+1:])
-            elif "Input()" in line:
-                input()
-            elif "Clear()" in line:
-                clear()
-            elif "import" in line:
-                os.system("cd " + os.getcwd() + " & " + "python ezcode.py " + line[len("import"):])
-            else:
-                print("No program called " + line + " assuming it is okay")
-
-def execodeline(code):
-    global x
-    global variables
-    x += 1
-    sys.setrecursionlimit(x)
-    line = str(code)
-    if str(line).find("Write(") != -1 and line[6] == '"' and line[len(line) - 2] == '"':
-        print(line[7:len(line) - 2])
-    elif str(line).find("Write(") != -1 :
-        calc = str(line)[6:len(str(line))-1]
-        result = "error, faulty equation!"
-        if(calc in variables.keys()):
-            result = variables[calc]
-        else:
-            calc = calc.split(" ")
-            if (calc[1] == "+"):
-                result = int(calc[0]) + int(calc[2])
-            elif (calc[1] == "-"):
-                result = int(calc[0]) - int(calc[2])
-            elif (calc[1] == "*"):
-                result = int(calc[0]) * int(calc[2])
-            elif (calc[1] == "/"):
-                result = int(calc[0]) / int(calc[2])
-            elif (calc[1] == "^"):
-                result = int(calc[0]) ** int(calc[2])
-        print(result)
-    elif "ifequal" in line:
-        lineForWork = line[len("if"):].split("[")[1].split(",")
-        if (int(variables[lineForWork[0]]) == int(lineForWork[1])):
-            execodeline(lineForWork[2][1:len(lineForWork[2])-1])
-    elif "ifless" in line:
-        lineForWork = line[len("if"):].split("[")[1].split(",")
-        if (int(variables[lineForWork[0]]) < int(lineForWork[1])):
-            execodeline(lineForWork[2][1:len(lineForWork[2])-1])
-    elif line[:len(line)-2] in variables.keys() and "++" in line:
-        variables[line[:len(line)-2]] = int(variables[line[:len(line)-2]]) + 1
-    elif line[:len(line)-2] in variables.keys() and "-" in line:
-        variables[line[:len(line)-2]] = int(variables[line[:len(line)-2]]) - 1
-    elif line in funcs.keys():
-        #print(funcs[line])
-        for cmd in funcs[line]:
-            execodeline(cmd)
-    elif "Clear()" in line:
-        clear()
-    elif line == "":
-        pass
-    elif "import" in line:
-        print("import can only happen at definition level(outside of function), keep that in mind.")
+    if line[:len("function ")] == "function ":
+        variables[line[len("function "):].split("{")[0]] = []
+        variables[line[len("function "):].split("{")[0]].append("Function")
+        variables[line[len("function "):].split("{")[0]].append(line[len("function "):].split("{")[1].split("}")[0].split(":"))
+    elif line[:len("Write(")] == "Write(":
+        if (line[len("Write("):len(line)-1] in variables):
+            print(variables[line[len("Write("):len(line)-1]][0])
+        elif (line[len("Write("):][0] == "\""):
+            print(line[len("Write(") + 1:len(line)-2])
+    
+    elif line.split("(")[0] in variables:
+        func = line.split("(")[0]
+        if (variables[func][0] == "Function"):
+            for funcLine in variables[func][1]:
+                execLine(funcLine)
+    elif "<-" in line:
+        variables[line.split("<-")[0]] = [line.split("<-")[1]]
+    elif line[:len(line)-2] in variables and "++" in line:
+        variables[line[:len(line)-2]] = [int(variables[line[:len(line)-2]][0]) + 1]
+    elif line[:len(line)-2] in variables and "--" in line:
+        variables[line[:len(line)-2]] = [int(variables[line[:len(line)-2]][0]) - 1]
+    elif line[:len("python ")] == "python ":
+        eval(line[len("python "):])
     else:
-        print("." + line + ".")
-        print("No program called " + line + " assuming it is okay")
-        
-execode()
+        print("No command called \"" + line + "\", assuming it's okay")
+
+
+for codeLine in code:
+    execLine(codeLine)
