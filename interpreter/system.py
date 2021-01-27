104255380e54809e11f287983f48ccd6a1cd496b
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2021-01-27 21:40:15 +0100

    Create system.py

---
+++ interpreter/system.py
@@ -0,0 +1,14 @@
+def Write(text):
+    print(text)
+
+def Input():
+    return input()
+
+def Ask(question):
+    return input(question)
+
+def Welcome(name):
+    print("Hello, " + name)
+
+def AddString(string1, string2):
+    return str(string1) + str(string2)
